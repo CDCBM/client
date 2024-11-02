6257a57984f745b65256ce417a8340fe7e7f9c28
woojin <woojinchoi1023@gmail.com>
2024-11-02 22:05:05 +0900

    Fix: linter 오류 수정

--- src/pages/home/Nav.tsx
+++ src/pages/home/Nav.tsx
@@ -1,12 +1,10 @@
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import { Link } from "react-router-dom";
 
